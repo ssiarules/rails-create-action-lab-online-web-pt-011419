e7e4f5d92774ef130d01d83c2bb059f8e6e665dd
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-16 22:48:11 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,13 +11,13 @@ class StudentsController < ApplicationController
   def new
   end
 
-  def create
+def create
   @student = Student.new
   @student.first_name = params[:first_name]
   @student.last_name = params[:last_name]
   @student.save
   redirect_to student_path(@student)
-end 
+end
 
   end
 
